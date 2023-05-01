dd5f21cc05ba33459b5d452f2dceb6040f9d32fa
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-05-01 12:07:21 +0300

    update

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,4 +3,5 @@ import { contactsReducer } from './contactsSlice';
 
 export const store = configureStore({
   reducer: { contacts: contactsReducer },
+  devTools: process.env.NODE_ENV !== 'production',
 });
